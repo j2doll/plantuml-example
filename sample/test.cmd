7547e9a7ec3c4e849f139529be6f044d857f1d9f
j2doll <j2doll@gmail.com>
2017-08-12 23:47:07 +0900

    add sample files

--- sample/test.cmd
+++ sample/test.cmd
@@ -1,4 +1,13 @@
 java -jar plantuml.jar file1.txt
 java -jar plantuml.jar file2.txt
 java -jar plantuml.jar file3.txt
+java -jar plantuml.jar file4.txt
+java -jar plantuml.jar file5.txt
+java -jar plantuml.jar file6.txt
+java -jar plantuml.jar file7.txt
+java -jar plantuml.jar file8.txt
+java -jar plantuml.jar file9.txt
+java -jar plantuml.jar fileA.txt
+java -jar plantuml.jar fileB.txt
+java -jar plantuml.jar fileC.txt
 
